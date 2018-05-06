c93313c04c51ca4db6b5cde408170dc35a64b256
Ilya Semerukhin <semerukhin@gmail.com>
2018-05-06 15:50:29 +0300

    Fix code indentation

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,18 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	var deck = PlayingCardDeck()
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-//		for _ in 1...10 {
-//			if let card = deck.draw() {
-//				print("\(card)")
-//			}
-//		}
-	}
-
+   
+   var deck = PlayingCardDeck()
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      for _ in 1...10 {
+         if let card = deck.draw() {
+            print("\(card)")
+         }
+      }
+   }
+   
 }
 
 
