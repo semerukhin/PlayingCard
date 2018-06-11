cad415d5656efb78774dbb7da6648a5d0e314247
Ilya Semerukhin <semerukhin@gmail.com>
2018-06-12 00:32:53 +0300

    Create 6 random pairs of cards

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -10,34 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
    
-   var deck = PlayingCardDeck()
+   private var deck = PlayingCardDeck()
    
-   @IBOutlet weak var playingCardView: PlayingCardView! {
-      didSet {
-         let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-         swipe.direction = [.left, .right]
-         playingCardView.addGestureRecognizer(swipe)
-         let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-         playingCardView.addGestureRecognizer(pinch)
-      }
-   }
+   @IBOutlet private var cardViews: [PlayingCardView]!
    
-   @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-      switch sender.state {
-      case .ended: playingCardView.isFaceUp = !playingCardView.isFaceUp
-      default: break
-      }
-   }
-   
-   @objc func nextCard() {
-      if let card = deck.draw() {
-         playingCardView.rank = card.rank.order
-         playingCardView.suit = card.suit.rawValue
-      }
-   }
+//   @IBOutlet weak var playingCardView: PlayingCardView! {
+//      didSet {
+//         let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+//         swipe.direction = [.left, .right]
+//         playingCardView.addGestureRecognizer(swipe)
+//         let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+//         playingCardView.addGestureRecognizer(pinch)
+//      }
+//   }
+//
+//   @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+//      switch sender.state {
+//      case .ended: playingCardView.isFaceUp = !playingCardView.isFaceUp
+//      default: break
+//      }
+//   }
+//
+//   @objc func nextCard() {
+//      if let card = deck.draw() {
+//         playingCardView.rank = card.rank.order
+//         playingCardView.suit = card.suit.rawValue
+//      }
+//   }
    
    override func viewDidLoad() {
       super.viewDidLoad()
+      var cards = [PlayingCard]()
+      for _ in 1...((cardViews.count+1)/2) {
+         let card = deck.draw()!
+         cards += [card, card]
+      }
+      for cardView in cardViews {
+         cardView.isFaceUp = true
+         let card = cards.remove(at: cards.count.arc4random)
+         cardView.rank = card.rank.order
+         cardView.suit = card.suit.rawValue
+      }
 //      for _ in 1...52 {
 //         if let card = deck.draw() { print("\(card)") }
 //      }
