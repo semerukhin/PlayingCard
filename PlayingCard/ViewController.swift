ad16e3762f637227a27c727c5a94c42b89bf7ad8
Ilya Semerukhin <semerukhin@gmail.com>
2018-06-12 20:59:19 +0300

    Fix a bug with the disappearance of several playing cards after a match

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
    lazy var cardBehavior = CardBehavior(in: animator)
    
    private var faceUpCardViews: [PlayingCardView] {
-      return cardViews.filter{ $0.isFaceUp && !$0.isHidden }
+      return cardViews.filter{ $0.isFaceUp && !$0.isHidden && $0.transform != CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0) && $0.alpha == 1 }
    }
    
    private var faceUpCardViewsMatch: Bool {
@@ -31,32 +31,33 @@ class ViewController: UIViewController {
    @objc func flipCard(_ recognizer: UITapGestureRecognizer) {
       switch recognizer.state {
       case .ended:
-         if let chosenCardView = recognizer.view as? PlayingCardView {
+         if let chosenCardView = recognizer.view as? PlayingCardView, faceUpCardViews.count < 2 {
             cardBehavior.removeItem(chosenCardView)
             UIView.transition(with: chosenCardView, duration: 0.6, options: [.transitionFlipFromLeft], animations: {
                chosenCardView.isFaceUp = !chosenCardView.isFaceUp
             }, completion: { finished in
+               let cardsToAnimate = self.faceUpCardViews
                if self.faceUpCardViewsMatch {
                   UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.6, delay: 0, options: [], animations: {
-                     self.faceUpCardViews.forEach {
+                     cardsToAnimate.forEach {
                         $0.transform = CGAffineTransform.identity.scaledBy(x: 3.0, y: 3.0)
                      }
                   }, completion: { position in
                      UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.75, delay: 0, options: [], animations: {
-                        self.faceUpCardViews.forEach {
+                        cardsToAnimate.forEach {
                            $0.transform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
                            $0.alpha = 0
                         }
                      }, completion: { position in
-                        self.faceUpCardViews.forEach {
+                        cardsToAnimate.forEach {
                            $0.isHidden = true
                            $0.alpha = 1
                            $0.transform = .identity
                         }
                      })
                   })
-               } else if self.faceUpCardViews.count == 2 {
-                  self.faceUpCardViews.forEach { cardView in
+               } else if cardsToAnimate.count == 2 {
+                  cardsToAnimate.forEach { cardView in
                      UIView.transition(with: cardView, duration: 0.6, options: [.transitionFlipFromLeft], animations: {
                         cardView.isFaceUp = false
                      }, completion: { finished in
