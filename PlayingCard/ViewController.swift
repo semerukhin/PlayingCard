890c6926a5b42107461765657464bcf8e9d16263
Ilya Semerukhin <semerukhin@gmail.com>
2018-06-12 16:05:38 +0300

    Force the playing cards move

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -14,6 +14,24 @@ class ViewController: UIViewController {
    
    @IBOutlet private var cardViews: [PlayingCardView]!
    
+   lazy var animator = UIDynamicAnimator(referenceView: view)
+   
+   lazy var collisionBehavior: UICollisionBehavior = {
+      let behavior = UICollisionBehavior()
+      behavior.translatesReferenceBoundsIntoBoundary = true
+      animator.addBehavior(behavior)
+      return behavior
+   }()
+   
+   lazy var itemBehavior: UIDynamicItemBehavior = {
+      let behavior = UIDynamicItemBehavior()
+      behavior.allowsRotation = false
+      behavior.elasticity = 1.0
+      behavior.resistance = 0
+      animator.addBehavior(behavior)
+      return behavior
+   }()
+   
    private var faceUpCardViews: [PlayingCardView] {
       return cardViews.filter{ $0.isFaceUp && !$0.isHidden }
    }
@@ -77,36 +95,57 @@ class ViewController: UIViewController {
          cardView.rank = card.rank.order
          cardView.suit = card.suit.rawValue
          cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+         collisionBehavior.addItem(cardView)
+         itemBehavior.addItem(cardView)
+         let push = UIPushBehavior(items: [cardView], mode: .instantaneous)
+         push.angle = (2*CGFloat.pi).arc4random
+         push.magnitude = CGFloat(1.0) + CGFloat(2.0).arc4random
+         push.action = { [unowned push] in
+            push.dynamicAnimator?.removeBehavior(push)
+         }
+         animator.addBehavior(push)
       }
-      //      for _ in 1...52 {
-      //         if let card = deck.draw() { print("\(card)") }
-      //      }
+//      for _ in 1...52 {
+//         if let card = deck.draw() { print("\(card)") }
+//      }
    }
    
-   //   @IBOutlet weak var playingCardView: PlayingCardView! {
-   //      didSet {
-   //         let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-   //         swipe.direction = [.left, .right]
-   //         playingCardView.addGestureRecognizer(swipe)
-   //         let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-   //         playingCardView.addGestureRecognizer(pinch)
-   //      }
-   //   }
-   //
-   //   @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-   //      switch sender.state {
-   //      case .ended: playingCardView.isFaceUp = !playingCardView.isFaceUp
-   //      default: break
-   //      }
-   //   }
-   //
-   //   @objc func nextCard() {
-   //      if let card = deck.draw() {
-   //         playingCardView.rank = card.rank.order
-   //         playingCardView.suit = card.suit.rawValue
-   //      }
-   //   }
+//   @IBOutlet weak var playingCardView: PlayingCardView! {
+//      didSet {
+//         let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+//         swipe.direction = [.left, .right]
+//         playingCardView.addGestureRecognizer(swipe)
+//         let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+//         playingCardView.addGestureRecognizer(pinch)
+//      }
+//   }
+//
+//   @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+//      switch sender.state {
+//      case .ended: playingCardView.isFaceUp = !playingCardView.isFaceUp
+//      default: break
+//      }
+//   }
+//
+//   @objc func nextCard() {
+//      if let card = deck.draw() {
+//         playingCardView.rank = card.rank.order
+//         playingCardView.suit = card.suit.rawValue
+//      }
+//   }
    
+}
+
+extension CGFloat {
+   var arc4random: CGFloat {
+      if self == 0 {
+         return 0
+      } else if self > 0 {
+         return CGFloat(arc4random_uniform(UInt32(self)))
+      } else {
+         return -CGFloat(arc4random_uniform(UInt32(abs(self))))
+      }
+   }
 }
 
 
